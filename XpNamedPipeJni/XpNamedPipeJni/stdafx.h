3b472280deec159b7e3e642736173ba5ef360a92
nsawadsky <nsawadsky@gmail.com>
2011-10-03 13:05:01 -0700

    Initial commit in new repository.

---
+++ XpNamedPipeJni/XpNamedPipeJni/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+#include <sddl.h>
+#include <vector>
+#include "jni.h"
+
+// Avoid warnings for use of throw as exception specification.
+#pragma warning( disable : 4290 )
