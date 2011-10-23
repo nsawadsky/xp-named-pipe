732ab1fefb69e495c0cff35f4a25e1bd98bef182
nsawadsky <nsawadsky@gmail.com>
2011-10-23 16:57:58 -0700

    Enhanced XpNamedPipe tests.

--- XpNamedPipeJava/test/xpnp/XpNamedPipeServer.java
+++ XpNamedPipeJava/test/xpnp/XpNamedPipeServer.java
@@ -20,38 +20,39 @@ public class XpNamedPipeServer implements Runnable {
             XpNamedPipe listeningPipe = XpNamedPipe.createNamedPipe("historyminer", true);
             System.out.println("Created listening pipe, listening for 1 second");
             
-            XpNamedPipe newConn = null;
-            try {
-                newConn = listeningPipe.acceptConnection(1000);
-            } catch (IOException e) {
-                System.out.println("Caught IOException: " + e);
+            while (true) {
+                XpNamedPipe newConn = null;
+                try {
+                    newConn = listeningPipe.acceptConnection(1000);
+                } catch (IOException e) {
+                    System.out.println("Caught IOException: " + e);
+                }
+                
+                System.out.println("Listening indefinitely");
+                newConn = listeningPipe.acceptConnection();
+                
+                System.out.println("Accepted connection");
+                
+                System.out.println("Waiting for 1 second for message");
+                try {
+                    newConn.readMessage(1000);
+                } catch (IOException e) {
+                    System.out.println("Caught IOException: " + e);
+                }
+                System.out.println("Waiting indefinitely for message");
+                byte[] buffer = newConn.readMessage();
+                
+                System.out.println("Got message: " + new String(buffer, "UTF-8"));
+                
+                System.out.println("Sending response");
+                newConn.writeMessage("Well, hi there!".getBytes("UTF-8"));
+                
+                Thread.sleep(2000);
+                
+                System.out.println("Closing pipe");
+                
+                newConn.close();
             }
-            
-            System.out.println("Listening indefinitely");
-            newConn = listeningPipe.acceptConnection();
-            
-            System.out.println("Accepted connection");
-            
-            System.out.println("Waiting for 1 second for message");
-            try {
-                newConn.readMessage(1000);
-            } catch (IOException e) {
-                System.out.println("Caught IOException: " + e);
-            }
-            System.out.println("Waiting indefinitely for message");
-            byte[] buffer = newConn.readMessage();
-            
-            System.out.println("Got message: " + new String(buffer, "UTF-8"));
-            
-            System.out.println("Sending response");
-            newConn.writeMessage("Well, hi there!".getBytes("UTF-8"));
-            
-            Thread.sleep(2000);
-            
-            System.out.println("Closing pipes");
-            
-            newConn.close();
-            listeningPipe.close();
         } catch (Exception e) {
             System.out.println("Server caught exception: " + e);
         }
