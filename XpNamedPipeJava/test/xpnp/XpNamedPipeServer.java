aa2e31bbb852ed9753b0b67cdcf92be6df7605d8
nsawadsky <nsawadsky@gmail.com>
2011-10-23 13:40:35 -0700

    Improved test for XpNamedPipe.

--- XpNamedPipeJava/test/xpnp/XpNamedPipeServer.java
+++ XpNamedPipeJava/test/xpnp/XpNamedPipeServer.java
@@ -1,5 +1,7 @@
 package xpnp;
 
+import java.io.IOException;
+
 import xpnp.XpNamedPipe;
 
 public class XpNamedPipeServer implements Runnable {
@@ -16,15 +18,39 @@ public class XpNamedPipeServer implements Runnable {
     public void run() {
         try {
             XpNamedPipe listeningPipe = XpNamedPipe.createNamedPipe("historyminer", true);
-            System.out.println("Created listening pipe");
+            System.out.println("Created listening pipe, listening for 1 second");
             
-            XpNamedPipe newConn = listeningPipe.acceptConnection();
+            XpNamedPipe newConn = null;
+            try {
+                newConn = listeningPipe.acceptConnection(1000);
+            } catch (IOException e) {
+                System.out.println("Caught IOException: " + e);
+            }
+            
+            System.out.println("Listening indefinitely");
+            newConn = listeningPipe.acceptConnection();
+            
             System.out.println("Accepted connection");
             
             byte[] buffer = new byte[1024];
+            
+            System.out.println("Waiting for 1 second for message");
+            try {
+                newConn.read(buffer, 1000);
+            } catch (IOException e) {
+                System.out.println("Caught IOException: " + e);
+            }
+            System.out.println("Waiting indefinitely for message");
             newConn.read(buffer);
             
             System.out.println("Got message: " + new String(buffer, "UTF-8"));
+            
+            System.out.println("Sending response");
+            newConn.write("Well, hi there!".getBytes("UTF-8"));
+            
+            Thread.sleep(2000);
+            
+            System.out.println("Closing pipes");
             
             newConn.close();
             listeningPipe.close();
