0db85f410a65b8465438c9dfc56c628c96f82950
nsawadsky <nsawadsky@localhost>
2012-03-05 12:42:00 -0800

    Added simple test for named pipe ACL's.

---
+++ XpNamedPipeJava/test/xpnp/TestXpnpSecurity.java
@@ -0,0 +1,25 @@
+package xpnp;
+
+import java.io.IOException;
+
+public class TestXpnpSecurity {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        XpNamedPipe pipe = null;
+        try {
+            pipe = XpNamedPipe.openNamedPipe("S-1-5-21-3327237310-981301597-340182190-1006\\reverb-index", false);
+            System.out.println("Opened pipe successfully");
+        } catch (IOException e) {
+            System.out.println("Error opening named pipe: " + e);
+        } finally {
+            if (pipe != null) {
+                pipe.close();
+            }
+        }
+
+    }
+
+}
